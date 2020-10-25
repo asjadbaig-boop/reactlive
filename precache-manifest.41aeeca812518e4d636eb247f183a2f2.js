234b5409f22c56703ec5e42967e01bcb8db6a34b
Asjad Baig <asjadbaigqaz@gmail.com>
2020-10-25 11:35:03 +0530

    Updates

---
+++ precache-manifest.41aeeca812518e4d636eb247f183a2f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762933a0594c210885825f18c7b7e002",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "41b02f37541465a8e81d",
+    "url": "/reactlive/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "90a78ed9c07c495515f5",
+    "url": "/reactlive/static/css/main.1f3d8705.chunk.css"
+  },
+  {
+    "revision": "41b02f37541465a8e81d",
+    "url": "/reactlive/static/js/2.6dfc62e7.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/reactlive/static/js/2.6dfc62e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a78ed9c07c495515f5",
+    "url": "/reactlive/static/js/main.3b0c7ec6.chunk.js"
+  },
+  {
+    "revision": "38c03d03eb7e7c98b14f",
+    "url": "/reactlive/static/js/runtime-main.aba0c710.js"
+  },
+  {
+    "revision": "32ca5664fa07d6a16ac3f8c72d502447",
+    "url": "/reactlive/static/media/about.32ca5664.png"
+  },
+  {
+    "revision": "99b4bcad840a8acb13c5a633cef65bed",
+    "url": "/reactlive/static/media/digitalmarketing.99b4bcad.jpg"
+  },
+  {
+    "revision": "4fa2f6146fef301a550b103fe26d89dc",
+    "url": "/reactlive/static/media/fullstack.4fa2f614.jpg"
+  },
+  {
+    "revision": "8733dd323d4fdf1af63cd21927899b7d",
+    "url": "/reactlive/static/media/me.png.8733dd32.png"
+  },
+  {
+    "revision": "4d958e9056e59065dd9c47b19bd6f7f1",
+    "url": "/reactlive/static/media/node.4d958e90.jpg"
+  },
+  {
+    "revision": "127b470a1814ca2ae06d9f40df34f190",
+    "url": "/reactlive/static/media/react.127b470a.png"
+  }
+]);
